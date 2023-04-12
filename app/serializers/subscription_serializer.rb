ea7c494690cc376beaf4898cdff735099d6d514f
Brandon Gray <brandonjoe88@hotmail.com>
2023-04-12 08:03:28 -0600

    feat: adds create & update to subscriptions

---
+++ app/serializers/subscription_serializer.rb
@@ -0,0 +1,36 @@
+class SubscriptionSerializer
+  def self.subscription(subscription)
+    {
+      "data": 
+        { 
+          "type": subscription.class.to_s.downcase,
+          "id":   subscription.id,
+          "attributes": 
+            {
+                          "title":     subscription.title,
+                          "price":     subscription.price,
+                          "status":    subscription.status,
+                          "frequency": subscription.frequency
+            }
+        }
+    }
+  end
+
+  def self.subscriptions(subscriptions)
+    {
+      "data": subscriptions.map do |subscription|
+        { 
+          "type": subscription.class.to_s.downcase,
+          "id":   subscription.id,
+          "attributes": 
+            {
+                          "title":     subscription.title,
+                          "price":     subscription.price,
+                          "status":    subscription.status,
+                          "frequency": subscription.frequency
+            }
+        }
+      end
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :subscriptions, only: [:index]
+      resources :subscriptions, only: [:create, :index, :update]
     end
   end
 end
